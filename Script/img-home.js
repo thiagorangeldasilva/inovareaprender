be724715c781626834d2658ed4d682a9d2c7e764
Thiago Rangel <thiagodasilva925@gmail.com>
2020-08-09 19:54:01 -0300

    atualização do site
    fiz uma gambiarra para as engrenagens (irei corrigir), ajeitei os sliders

--- Script/img-home.js
+++ Script/img-home.js
@@ -1,42 +1,39 @@
-let largura = innerWidth, img1filhos = document.getElementById('img1').children, frames = 0, indice
+let largura, img1filhos = document.getElementById('img1').children, img2filhos = document.getElementById('img2').children
 
 function atualiza(){
-    frames++
-    if(largura < 400){
-        indice = 2
-    }else if(largura < 500){
-        indice = 3
-    }else if(largura < 600){
-        indice = 4
-    }else if(largura < 900){
-        indice = 6
-    }else if(largura < 1200){
-        indice = 9
-    }else if(largura < 1700){
-        indice = 12
-    }else if(largura < 600){
-        indice = 4
-    }else if(largura < 2000){
-        indice = 13
-    }
+    //adiciona
+    if(largura < 440){ img1filhos[2].classList.add('ocultar'); img2filhos[2].classList.add('ocultar') }
+    if(largura < 560){ img1filhos[3].classList.add('ocultar'); img2filhos[3].classList.add('ocultar') }
+    if(largura < 715){ img1filhos[4].classList.add('ocultar'); img2filhos[4].classList.add('ocultar') }
+    if(largura < 840){ img1filhos[5].classList.add('ocultar'); img2filhos[5].classList.add('ocultar') }
+    if(largura < 1000){ img1filhos[6].classList.add('ocultar'); img2filhos[6].classList.add('ocultar') }
+    if(largura < 1120){ img1filhos[7].classList.add('ocultar'); img2filhos[7].classList.add('ocultar') }
+    if(largura < 1275){ img1filhos[8].classList.add('ocultar'); img2filhos[8].classList.add('ocultar') }
+    if(largura < 1400){ img1filhos[9].classList.add('ocultar'); img2filhos[9].classList.add('ocultar') }
+    if(largura < 1565){ img1filhos[10].classList.add('ocultar'); img2filhos[10].classList.add('ocultar') }
+    if(largura < 1690){ img1filhos[11].classList.add('ocultar'); img2filhos[11].classList.add('ocultar') }
+    if(largura < 1840){ img1filhos[12].classList.add('ocultar'); img2filhos[12].classList.add('ocultar') }
+    if(largura < 2000){ img1filhos[13].classList.add('ocultar'); img2filhos[13].classList.add('ocultar') }
+    //remove
+    if(largura > 440){ img1filhos[2].classList.remove('ocultar'); img2filhos[2].classList.remove('ocultar') }
+    if(largura > 560){ img1filhos[3].classList.remove('ocultar'); img2filhos[3].classList.remove('ocultar') }
+    if(largura > 715){ img1filhos[4].classList.remove('ocultar'); img2filhos[4].classList.remove('ocultar') }
+    if(largura > 840){ img1filhos[5].classList.remove('ocultar'); img2filhos[5].classList.remove('ocultar') }
+    if(largura > 1000){ img1filhos[6].classList.remove('ocultar'); img2filhos[6].classList.remove('ocultar') }
+    if(largura > 1120){ img1filhos[7].classList.remove('ocultar'); img2filhos[7].classList.remove('ocultar') }
+    if(largura > 1275){ img1filhos[8].classList.remove('ocultar'); img2filhos[8].classList.remove('ocultar') }
+    if(largura > 1400){ img1filhos[9].classList.remove('ocultar'); img2filhos[9].classList.remove('ocultar') }
+    if(largura > 1565){ img1filhos[10].classList.remove('ocultar'); img2filhos[10].classList.remove('ocultar') }
+    if(largura > 1690){ img1filhos[11].classList.remove('ocultar'); img2filhos[11].classList.remove('ocultar') }
+    if(largura > 1840){ img1filhos[12].classList.remove('ocultar'); img2filhos[12].classList.remove('ocultar') }
+    if(largura > 2000){ img1filhos[13].classList.remove('ocultar'); img2filhos[13].classList.remove('ocultar') }
 }
-function desenhaADD(elemento){
-    for(;elemento < img1filhos.length; elemento++){
-        img1filhos[elemento].classList.add('ocultar')
-    }
-}
-function desenhaRemove(elemento){
-    for(;elemento > 1; elemento--){
-        img1filhos[elemento].classList.remove('ocultar')
-    }
-}
-
 
 function roda(){
-    atualiza()
-    desenhaADD(indice)
-    desenhaRemove(indice)
-    window.requestAnimationFrame(roda)
+    setInterval( () => {
+        largura = innerWidth
+        atualiza()
+    }, 30)
 }
 
 roda()
